eb1a80863255a64ee51b11b14650afd65a2c4993
Raulx103 <raulx103@gmail.com>
2024-04-13 18:29:32 +0530

    ringloader component added

--- frontend/src/pages/AdminPages/AdminLoginPage.jsx
+++ frontend/src/pages/AdminPages/AdminLoginPage.jsx
@@ -3,15 +3,20 @@ import { useLoginAdminMutation } from "../../store";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+import RingLoader from "../../components/RingLoader";
 
 import { setAdminAuthentication } from "../../store";
 
 function AdminLoginPage() {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const [errorMessage, setErrorMessage] = useState("");
   const Navigate = useNavigate();
   const dispatch = useDispatch();
-  const [loginAdmin] = useLoginAdminMutation();
+  const [loginAdmin, results] = useLoginAdminMutation();
 
   const onSubmit = async (data) => {
     const res = await loginAdmin({ id: data.id, password: data.password });
@@ -32,16 +37,23 @@ function AdminLoginPage() {
           className="border-2 rounded p-2"
           {...register("id", { required: true })}
         />
-
+        {errors.id && (
+          <span className="text-red-500">This field is required</span>
+        )}
         <input
           placeholder="password"
           type="password"
           className=" border-2 rounded p-2"
           {...register("password", { required: true })}
         />
+        {errors.password && (
+          <span className="text-red-500">This field is required</span>
+        )}
+
         <p className="text-red-500 font-bold">{errorMessage}</p>
         <input className="bg-black rounded p-2 text-white" type="submit" />
       </form>
+      {results.isLoading ? <RingLoader /> : null}
     </div>
   );
 }
