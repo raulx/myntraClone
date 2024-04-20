e88dd42b0f0ae0ac09bbc5f8fa22a40c0eedf734
Raulx103 <raulx103@gmail.com>
2024-04-20 20:49:42 +0530

    backend: editController fully functional

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -24,27 +24,37 @@ const editProduct = asyncHandler(async (req, res, next) => {
 
   try {
     let updatedData;
+    if (field === "specifications" || field === "product_details") {
+      // while updating specifications ,updated_value must be a json object in this format {key,value} where key is actually the field that needs to be updated.
+      // while updating product_details,updated_value must be a json object in this format {key:[]} where key is actually the field that needs to be updated.
+      let parsed_updated_value;
 
-    switch (field) {
-      case "product_details":
-        updatedData = { d: "Product details changed." };
-      case "specifications":
-        updatedData = { d: "specifications  updated" };
-        break;
+      // validate updated_value as a json
+      try {
+        parsed_updated_value = JSON.parse(updated_value);
+      } catch (err) {
+        res.status(400);
+        throw new Error("BAD REQUEST");
+      }
 
-      default:
-        if (!Product.schema.path(field)) {
-          res.status(400);
-          throw new Error("BAD REQUEST:Invalid field");
-        } else {
-          updatedData = await Product.findOneAndUpdate(
-            { product_id: product_id },
-            { [field]: updated_value },
-            { new: true }
-          );
-        }
+      updatedData = await Product.findOneAndUpdate(
+        { product_id: product_id },
+        { [field]: { [parsed_updated_value.key]: parsed_updated_value.value } },
+        { new: true }
+      );
+    } else {
+      // edgecase: validate if invalid field is sent in request.
+      if (!Product.schema.path(field)) {
+        res.status(400);
+        throw new Error("BAD REQUEST:Invalid field");
+      } else {
+        updatedData = await Product.findOneAndUpdate(
+          { product_id: product_id },
+          { [field]: updated_value },
+          { new: true }
+        );
+      }
     }
-
     res.json({ message: "updated successfully", updated_data: updatedData });
   } catch (err) {
     next(err);
