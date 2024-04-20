d8edf88010fdc7b67175279dff71ca4f91f76b61
Raulx103 <raulx103@gmail.com>
2024-04-20 14:08:55 +0530

    backend: redesigned the product schema.

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -3,18 +3,13 @@ import asyncHandler from "express-async-handler";
 
 const addProduct = asyncHandler(async (req, res, next) => {
   const { userId } = req.token;
-  const { title, brand, product_id, MRP, seller_name, subject, writeto } =
-    req.body;
+  const { title, brand, product_id, MRP } = req.body;
   const newData = {
     title: title,
     brand: brand,
     product_id: product_id,
     MRP: MRP,
     added_by: userId,
-    seller: {
-      name: seller_name,
-      grievanceredressal: { subject: subject, writeto: writeto },
-    },
   };
   try {
     const newProduct = await Product.create(newData);
@@ -24,4 +19,31 @@ const addProduct = asyncHandler(async (req, res, next) => {
   }
 });
 
-export { addProduct };
+const editProduct = asyncHandler(async (req, res, next) => {
+  const { product_id, field, updated_value } = req.body;
+
+  try {
+    let updatedData;
+
+    switch (field) {
+      case "product_details":
+        updatedData = { d: "Product details changed." };
+      case "specifications":
+        updatedData = { d: "this is updated data" };
+        break;
+
+      default:
+        updatedData = await Product.findOneAndUpdate(
+          { product_id: product_id },
+          { [field]: updated_value },
+          { new: true }
+        );
+    }
+
+    res.json({ message: "updated successfully", updated_data: updatedData });
+  } catch (err) {
+    next(err);
+  }
+});
+
+export { addProduct, editProduct };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import verifyAdmin from "../middlewares/verifyAdmin.js";
-import { addProduct } from "../controllers/productController.js";
+import { addProduct, editProduct } from "../controllers/productController.js";
 const productRouter = express.Router();
 
 productRouter.route("/add").post(verifyAdmin, addProduct);
+productRouter.route("/edit").patch(verifyAdmin, editProduct);
 
 export default productRouter;
