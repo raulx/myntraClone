2cef5366046a24e0dbf4d8a02deb444fa0137508
Raulx103 <raulx103@gmail.com>
2024-04-23 20:21:46 +0530

    frontend:active effect added to admin routes

--- frontend/src/pages/Admin/AdminHomePage.jsx
+++ frontend/src/pages/Admin/AdminHomePage.jsx
@@ -1,35 +1,82 @@
 import { Navbar, Image } from "react-bootstrap";
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLocation } from "react-router-dom";
 import { TypographyH3 } from "../../components/Typography/Typography.jsx";
 import { FaBuildingColumns, FaBox } from "react-icons/fa6";
 import { FiBox } from "react-icons/fi";
 import { VscInsert } from "react-icons/vsc";
+import { useState, useEffect } from "react";
 import { TbDeviceAnalytics } from "react-icons/tb";
+
 function AdminHomePage() {
+  const location = useLocation();
+  const [activeLink, setActiveLink] = useState("");
+  console.log(location.pathname);
+  useEffect(() => {
+    // Set active link based on current route
+    switch (location.pathname) {
+      case "/page/admin/products":
+        setActiveLink("products");
+        break;
+      case "/page/admin/products/insert":
+        setActiveLink("insert");
+        break;
+      case "/page/admin/products/analytics":
+        setActiveLink("analytics");
+        break;
+      case "/page/admin/products/orders":
+        setActiveLink("orders");
+        break;
+      default:
+        setActiveLink("");
+        break;
+    }
+  }, [location]);
   return (
     <div className="xl:block hidden h-screen overflow-hidden">
       <div>
         <AdminPageNavbar />
       </div>
       <div className="flex h-full">
-        <div className=" border-r-2 px-2 py-4">
-          <div className="flex gap-2 items-center my-8 cursor-pointer font-bold ">
-            <FiBox />
+        <div className=" border-r-2 px-2 py-4 w-44">
+          <div
+            className={`flex gap-2 items-center my-8 cursor-pointer font-bold ${
+              activeLink === "products" && "font-extrabold"
+            } `}
+          >
+            <FiBox
+              className={` ${
+                activeLink === "products"
+                  ? "text-red-500 scale-110 text-2xl"
+                  : "text-xl"
+              }`}
+            />
             <Link to={"/page/admin/products"}>See Products</Link>
           </div>
 
-          <div className="flex gap-2 items-center my-8 cursor-pointer font-bold ">
-            <VscInsert />
+          <div
+            className={`flex gap-2 items-center my-8 cursor-pointer font-bold ${
+              activeLink === "insert" && "font-extrabold"
+            } `}
+          >
+            <VscInsert className="text-xl" />
             <Link to={"/page/admin/products/insert"}>Insert Product</Link>
           </div>
 
-          <div className="flex gap-2 items-center my-8 cursor-pointer font-bold ">
-            <TbDeviceAnalytics />
+          <div
+            className={`flex gap-2 items-center my-8 cursor-pointer font-bold ${
+              activeLink === "analytics" && "font-extrabold"
+            } `}
+          >
+            <TbDeviceAnalytics className="text-xl" />
             <Link to={"/page/admin/products/analytics"}>Analytics</Link>
           </div>
 
-          <div className="flex gap-2 items-center my-8 cursor-pointer font-bold ">
-            <FaBox />
+          <div
+            className={`flex gap-2 items-center my-8 cursor-pointer font-bold ${
+              activeLink === "orders" && "font-extrabold"
+            } `}
+          >
+            <FaBox className="text-xl" />
             <Link to={"/page/admin/products/orders"}>Orders</Link>
           </div>
         </div>
